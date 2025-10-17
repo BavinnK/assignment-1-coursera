89fb41d9364bdea4ecfd5acec1641afa8ed3f76c
bavin <bavinkurd9@gmail.com>
2025-10-17 23:21:22 +0300

    cleaned and organised the code abit to be more readable

--- assigment1.c
+++ assigment1.c
@@ -1,8 +1,26 @@
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//INCLUDES
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <stdio.h>
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//GLOBAL VARS
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 unsigned char arr_num []={23,90,2,46,4,77,89,34,1,102,111,11};
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//PROTOTYPES
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+unsigned char find_maximum(unsigned char *ptr,int size);
+unsigned char find_minimum(unsigned char *ptr,int size);
+void sort_array(unsigned char *ptr,int size);
+void print_array(unsigned char *ptr,int size);
+void print_array(unsigned char *ptr,int size);
+void print_statistics(void);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//FUNCTIONS
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //fining max in the array
-char find_maximum(unsigned char *ptr,int size){
+unsigned char find_maximum(unsigned char *ptr,int size){
     unsigned char max=0;
     for (int i = 0; i < size; i++)
     {
@@ -17,7 +35,7 @@ char find_maximum(unsigned char *ptr,int size){
 
 
 //finding min number in the array
-char find_minimum(unsigned char *ptr,int size){
+unsigned char find_minimum(unsigned char *ptr,int size){
     unsigned char min=*ptr;
     for (int i = 0; i < size; i++)
     {
@@ -29,6 +47,8 @@ char find_minimum(unsigned char *ptr,int size){
     }
     return min;
 }// end min func
+
+
 //signed char find_median() {}
 //for sort array func i used bubble sort algorithm
 void sort_array(unsigned char *ptr,int size){
@@ -50,6 +70,7 @@ void sort_array(unsigned char *ptr,int size){
     }
     
 }
+//print array func
 void print_array(unsigned char *ptr,int size){
     
     for (int i = 0; i < size; i++)
@@ -58,19 +79,27 @@ void print_array(unsigned char *ptr,int size){
     }
     printf("\n");
     
-}
+}//end print array
 
 
-int main(void){
+//print everything func
+void print_statistics(void) {
     int size = (sizeof(arr_num)/sizeof(arr_num[0]));
     printf("Before sorting: ");
     print_array(arr_num,size);
-
+    printf("--------------------------------------------------------------------\n");
     sort_array(arr_num,size);
     printf("after sorting:  ");
     print_array(arr_num,size);
     unsigned char max_num = find_maximum(arr_num,size);
     unsigned char min_num = find_minimum(arr_num,size);
     printf("\nmax:%d\nmin:%d\n",max_num,min_num);
+}//end of print_statistic
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//MAIN
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main(void){
+    print_statistics();
     return 0;
 }
