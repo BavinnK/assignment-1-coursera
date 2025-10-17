46ebada3aa6bf322120a3d26998dbe58448c8c33
bavin <bavinkurd9@gmail.com>
2025-10-17 21:59:28 +0300

    implemented both min and max funcs

--- assigment1.c
+++ assigment1.c
@@ -1,6 +1,34 @@
 
 #include <stdio.h>
+unsigned char arr_num []={23,90,2,46,4,77,89,34,102,111,11};
+char max(unsigned char *ptr,int size){
+    unsigned char max=0;
+    for (int i = 0; i < size; i++)
+    {
+        if (*ptr > max)
+        {
+            max = *ptr;
+        }
+        ptr++;   
+    }
+    return max;
+}
+char min(unsigned char *ptr,int size){
+    unsigned char max=0;
+    for (int i = 0; i < size; i++)
+    {
+        if (*ptr < max)
+        {
+            max = *ptr;
+        }
+        ptr++;   
+    }
+    return max;
+}
 int main(void){
-    printf("hello bavin");
+    int size = (sizeof(arr_num)/sizeof(arr_num[0]));
+    unsigned char max_num = max(arr_num,size);
+    unsigned char min_num = min(arr_num,size);
+    printf("max:%d/nmin:%d",max_num,min_num);
     return 0;
 }
