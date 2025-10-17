2804cede87b5bdc69a1a591cf507ce11a36b7c59
bavin <bavinkurd9@gmail.com>
2025-10-17 22:56:12 +0300

    implemented the print array and also the sorting array func i used buble sort algrithm

--- assigment1.c
+++ assigment1.c
@@ -1,7 +1,8 @@
 
 #include <stdio.h>
-unsigned char arr_num []={23,90,2,46,4,77,89,34,102,111,11};
-char max(unsigned char *ptr,int size){
+unsigned char arr_num []={23,90,2,46,4,77,89,34,1,102,111,11};
+//fining max in the array
+char find_maximum(unsigned char *ptr,int size){
     unsigned char max=0;
     for (int i = 0; i < size; i++)
     {
@@ -12,23 +13,64 @@ char max(unsigned char *ptr,int size){
         ptr++;   
     }
     return max;
-}
-char min(unsigned char *ptr,int size){
-    unsigned char max=0;
+}//end max func
+
+
+//finding min number in the array
+char find_minimum(unsigned char *ptr,int size){
+    unsigned char min=*ptr;
     for (int i = 0; i < size; i++)
     {
-        if (*ptr < max)
+        if (*ptr < min)
         {
-            max = *ptr;
+            min = *ptr;
         }
         ptr++;   
     }
-    return max;
+    return min;
+}// end min func
+//signed char find_median() {}
+//for sort array func i used bubble sort algorithm
+void sort_array(unsigned char *ptr,int size){
+
+    for (int i = 0; i < size-1; i++)
+    {
+        for (int  j = 0; j < size-1-i ; j++)
+        {
+            if (ptr[j] > ptr[j+1])
+            {
+                unsigned char temp=ptr[j];
+                ptr[j]=ptr[j+1];
+                ptr[j+1]=temp;
+            }
+            
+        }
+        
+        
+    }
+    
 }
+void print_array(unsigned char *ptr,int size){
+    
+    for (int i = 0; i < size; i++)
+    {
+        printf(" %d ",*ptr++);
+    }
+    printf("\n");
+    
+}
+
+
 int main(void){
     int size = (sizeof(arr_num)/sizeof(arr_num[0]));
-    unsigned char max_num = max(arr_num,size);
-    unsigned char min_num = min(arr_num,size);
-    printf("max:%d/nmin:%d",max_num,min_num);
+    printf("Before sorting: ");
+    print_array(arr_num,size);
+
+    sort_array(arr_num,size);
+    printf("after sorting:  ");
+    print_array(arr_num,size);
+    unsigned char max_num = find_maximum(arr_num,size);
+    unsigned char min_num = find_minimum(arr_num,size);
+    printf("\nmax:%d\nmin:%d\n",max_num,min_num);
     return 0;
 }
